57adce99a6539c13ccdb825e52245383c94ea926
NayanPacheco <REDE-002114>
2022-02-17 20:56:35 -0300

    criei-pastafuncoes

---
+++ modulo-01/funcoes/index.js
@@ -0,0 +1,80 @@
+// Exercícios Interpretação de Código
+//
+//    1- a) 10 ; 50
+//       b) Não aparece nada, erro no código
+
+//    2- a)  ele vai dizer se tem a palavra "cenoura" no texto escrito parta retorna true ou false e colocar em minusculo.
+//       b) i. true  ii. true   iii. true
+ 
+
+// Exercícios de escrita de código
+// 
+  //  1- a) 
+    //function funcao1()  {
+      //console.log ("Eu sou Nayan, tenho 27 anos, moro em Recife e sou estudante")
+     //}
+    // funcao1()
+
+    //b)  
+    
+     // function mostraTela(nome, idade, cidade, profissao){
+     //console.log (`Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissao} `) 
+        // }
+          // const nome = prompt ("digite seu nome")
+         // const idade = prompt ("digite sua idade")
+        // const cidade = prompt ("digite sua cidade")
+       // const profissao = prompt ("digite sua profissao")
+      // console.log(nome, idade, cidade, profissao)
+
+
+//  2- 
+    //function recebeNumeros (n1, n2) {
+   //let soma = n1 + n2
+  //return soma    
+ //}
+  //const n1 = Number (prompt(`Digite seu numero 1`))
+  //const n2 = Number (prompt(`Digite seu numero 2`))
+
+  //console.log (funcao2(n1, n2))
+ 
+// função funcao2_b(n1,n2) {
+// maior = n1 >= n2
+// return maior    
+// }
+// console.log (funcao2_b(10,10))
+
+// função funcao2_c(n1) {
+// par = (n1 % 2 === 0)
+// return o par    
+// }
+// console.log (funcao2_c(1))
+
+// função funcao2_d(string) {
+// tamanho = string.comprimento
+// maiusculo = string.toUpperCase()
+// console.log (`Tamanho da String é ${tamanho} e o nome em maiusculo fica ${maiusculo}`)
+// }
+// funcao2_d("anfdDFffSDG")   
+
+
+// 3 - 
+//function soma(n1, n2) {
+//  return n1 + n2
+//}
+//function subtracao(n1, n2) {
+//    return n1 - n2
+//}
+//function multiplicação(n1, n2) {
+//    return n1 * n2
+//}
+//function divisao(n1, n2) {
+//    return n1 / n2
+//}
+
+//const numero1 = Number(prompt("Digite um numero?"))
+//const numero2 = Number(prompt("Digite outro numero?"))
+
+//console.log("SOMA: ", soma(numero1, numero2))
+//console.log("SUBSTRAÇÃO: ", subtracao(numero1, numero2))
+//console.log("MULTIPLICAÇÃO: ", multiplicação(numero1, numero2))
+//console.log("DIVISÃO:", divisao(numero1, numero2))
