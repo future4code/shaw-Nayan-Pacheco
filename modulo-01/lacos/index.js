3ed6099285259ca0f83609e69a35a6a47f0b2c6e
NayanPacheco <REDE-002114>
2022-02-23 18:34:13 -0300

    fiz-atividade-lacos

---
+++ modulo-01/lacos/index.js
@@ -0,0 +1,72 @@
+//Exercícios de interpretação de código
+
+// 1- O código está propondo um loop com for, buscando valores para a somar e o resultado impresso é 10.
+
+// 2- a) Vai ser impresso os números 19, 21, 23, 25, 27 e 30.
+//    b) Sim, tirando a condição if e manter o console.log(numero).
+
+//3-  vai ser impresso 4 linhas com asteriscos.
+//    *
+//    **
+//    ***
+//    ****
+
+// Exercícios de escrita de código
+
+// 1- 
+// let i = 0
+
+// let nomeBicho =[]
+// let numerosBichos = Number(prompt("Quantos bichinhos de estimação você tem?"))
+// if (numerosBichos === 0){
+//    console.log("Que pena! Você pode adotar um pet!")
+// }
+// else{
+//     while(i <numerosBichos){
+//         nome = prompt("Me fale o nome do seu bichinho?")
+//         nomeBicho.push(nome)
+//         i++
+//     }
+// }
+// console.log(nomeBicho)
+
+// 2-
+// a) console.log(array)
+
+//    b) function dividirArray(array){
+//         for(let numeros of array){
+//           console.log(numero/10)    
+//   }    
+// }
+//  dividirArray(arrayOriginal)
+
+//    C) function restoArray(array) {
+//           cons novoArray =[]
+//           for (let numero of array) {
+//               if(numero % 2 === 0) {
+//                 novoArray.push(numero)
+//              }    
+//        }
+//        console.log(novaArray)
+//   } 
+// restoArray(arrayOriginal)
+
+//    d) function stringArray(array){
+//      const novoArray = [""]
+//      for(let i = 0; i < array.length; i++)
+//   }
+
+//    e) function descobreMaiorMenor(array) {
+//         let maior = array[0]
+//         let menor = array[0]
+//         for (number of array) {
+//             if (number > maior) {
+//                 maior = number
+//       }
+//             if (number < menor) {
+//                 menor = number
+//     }
+//   }
+//   console.log(maior)
+//   console.log(menor)    
+// }
